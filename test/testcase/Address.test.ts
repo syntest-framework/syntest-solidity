d5057fb7433dc9583b68370cc5d00dd580ae73e1
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-03-10 17:54:51 +0100

    Add basic test

---
+++ test/testcase/Address.test.ts
@@ -0,0 +1,27 @@
+import * as chai from "chai";
+import {
+    guessCWD,
+    loadConfig,
+    processConfig,
+    setupLogger,
+    setupOptions,
+} from "syntest-framework";
+import {Address} from "../../src";
+
+const expect = chai.expect;
+
+describe("AddressStatement", () => {
+    before(async () => {
+        await guessCWD(null);
+        await setupOptions("", "");
+        await loadConfig();
+        await processConfig({}, "");
+        await setupLogger();
+    });
+
+    it("should not have a null address", () => {
+        const statement = Address.getRandom();
+
+        expect(statement != null);
+    });
+});
