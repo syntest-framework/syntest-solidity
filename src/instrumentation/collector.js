dc7af6ca4df0435fe4110d7f6674783d21e28bfc
apanichella <a.panichella@tudelft.nl>
2021-03-23 21:23:59 +0100

    Instrumenting conditions in require statements

--- src/instrumentation/collector.js
+++ src/instrumentation/collector.js
@@ -42,7 +42,7 @@ class DataCollector {
         if (this.instrumentationData[hash]) {
           this.instrumentationData[hash].hits++;
 
-          if (this.instrumentationData[hash].type === "branch") {
+          if (this.instrumentationData[hash].type === "branch" || this.instrumentationData[hash].type === "requirePre") {
             this.instrumentationData[hash].left = this.lastComparison.left;
             this.instrumentationData[hash].right = this.lastComparison.right;
             this.instrumentationData[hash].opcode = this.lastComparison.opcode;
