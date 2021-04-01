b3b5200b7364a6efc475f03d4958a344a7d17617
apanichella <a.panichella@tudelft.nl>
2021-04-01 17:13:17 +0200

    Implementing execution results based on the interface from the framework
    Co-authored-by: Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>

--- src/search/SolidityExecutionResult.ts
+++ src/search/SolidityExecutionResult.ts
@@ -1,30 +1,29 @@
-import {
-    ExecutionResult,
-    Datapoint
-} from "syntest-framework";
+import { ExecutionResult, Datapoint } from "syntest-framework";
 
 export class SolidityExecutionResult implements ExecutionResult {
+  protected _dataPoints: Datapoint[];
 
-    protected _dataPoints: Datapoint[]
+  constructor(dataPoints: Datapoint[]) {
+    this._dataPoints = dataPoints;
+  }
 
-    constructor(dataPoints: Datapoint[]) {
-        this._dataPoints = dataPoints;
+  coversLine(line: number): boolean {
+    for (const trace of this._dataPoints){
+      if(trace.line === line && trace.hits>0)
+        return true;
     }
+    return false;
+  }
 
-    coversLine(line: number): boolean {
-        return false;
-    }
+  getTraces(): Datapoint[] {
+    return this._dataPoints;
+  }
 
-    getTraces(): Datapoint[] {
-        return this._dataPoints;
-    }
+  hasExceptions(): boolean {
+    return false;
+  }
 
-    hasExceptions(): boolean {
-        return false;
-    }
-
-    hasTimeout(): boolean {
-        return false;
-    }
-
+  hasTimeout(): boolean {
+    return false;
+  }
 }
