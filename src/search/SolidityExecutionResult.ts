6060d11e012c507ef81de23e5808f36c2b8c036c
apanichella <a.panichella@tudelft.nl>
2021-04-09 15:24:26 +0200

    Adding attributes for compatibility with the framework APIs

--- src/search/SolidityExecutionResult.ts
+++ src/search/SolidityExecutionResult.ts
@@ -6,20 +6,22 @@ export class SolidityExecutionResult implements ExecutionResult {
   constructor(dataPoints: Datapoint[]) {
     this._dataPoints = dataPoints;
     this._dataPoints.forEach((point) => {
-      if (point.type === "requirePost")
-        point.type = "probePost";
+      if (point.type === "requirePost") point.type = "probePost";
 
-      if (point.type === "requirePre")
-        point.type = "probePre";
-        //point..satisfied = true;
-    })
+      if (point.type === "requirePre") point.type = "probePre";
+      //point..satisfied = true;
+    });
   }
 
   coversLine(line: number): boolean {
     for (const trace of this._dataPoints) {
-      if ((trace.type === "statement" || trace.type === "function" || trace.type === "requirePre")
-          && trace.line === line
-          && trace.hits > 0)
+      if (
+        (trace.type === "statement" ||
+          trace.type === "function" ||
+          trace.type === "requirePre") &&
+        trace.line === line &&
+        trace.hits > 0
+      )
         return true;
     }
     return false;
