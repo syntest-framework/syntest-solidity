8348992b783fa5b814cb272b34d5a0120646b74e
apanichella <a.panichella@tudelft.nl>
2021-04-16 11:35:33 +0200

    Enabling speacial addresses

--- src/testcase/statements/AddressStatement.ts
+++ src/testcase/statements/AddressStatement.ts
@@ -10,11 +10,11 @@ import {
  * @author Dimitri Stallenberg
  */
 export class AddressStatement extends PrimitiveStatement<string> {
-  protected account: number;
+  private _account: number;
 
   constructor(type: string, uniqueId: string, value: string, account: number) {
     super(type, uniqueId, value);
-    this.account = account;
+    this._account = account;
   }
 
   mutate(sampler: TestCaseSampler, depth: number): AddressStatement {
@@ -23,30 +23,34 @@ export class AddressStatement extends PrimitiveStatement<string> {
         sampler.sampleStatement(depth, this.type, "primitive")
       );
     }
-    if (prng.nextBoolean || this.account === 0)
+    if (prng.nextBoolean)
       return new AddressStatement(
         this.type,
         prng.uniqueId(),
-        `accounts[${this.account + 1}]`,
-        this.account + 1
+        `accounts[${this._account + 1}]`,
+        this._account + 1
       );
     else
       return new AddressStatement(
         this.type,
         prng.uniqueId(),
-        `accounts[${this.account - 1}]`,
-        this.account - 1
+        `accounts[${this._account - 1}]`,
+        this._account - 1
       );
   }
 
   copy(): AddressStatement {
-    return new AddressStatement(this.type, this.id, this.value, this.account);
+    return new AddressStatement(this.type, this.id, this.value, this._account);
   }
 
   static getRandom(type = "address") {
-    const account = prng.nextInt(0, 5);
+    const account = prng.nextInt(-1, 5);
     const value = `accounts[${account}]`;
 
     return new AddressStatement(type, prng.uniqueId(), value, account);
+  }
+
+  get account(): number {
+    return this._account;
   }
 }
