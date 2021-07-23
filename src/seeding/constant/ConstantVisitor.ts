3c82a145481135eea7940bdeb7133dece96b53af
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-07-23 17:22:37 +0200

    Add constant pool and update the visitor to write values into it

--- src/seeding/constant/ConstantVisitor.ts
+++ src/seeding/constant/ConstantVisitor.ts
@@ -1,11 +1,8 @@
 import { SolidityVisitor } from "../../graph/SolidityVisitor";
 import {
-  SourceUnit,
   PragmaDirective,
   ImportDirective,
   ContractDefinition,
-  InheritanceSpecifier,
-  StateVariableDeclaration,
   UsingForDeclaration,
   StructDefinition,
   ModifierDefinition,
@@ -16,52 +13,17 @@ import {
   EnumDefinition,
   VariableDeclaration,
   UserDefinedTypeName,
-  Mapping,
-  ArrayTypeName,
   FunctionTypeName,
-  Block,
-  ExpressionStatement,
-  IfStatement,
-  WhileStatement,
-  ForStatement,
-  InlineAssemblyStatement,
-  DoWhileStatement,
-  ContinueStatement,
-  BreakStatement,
-  ReturnStatement,
-  EmitStatement,
-  ThrowStatement,
-  VariableDeclarationStatement,
   ElementaryTypeName,
-  AssemblyBlock,
-  AssemblyCall,
-  AssemblyLocalDefinition,
-  AssemblyAssignment,
-  AssemblyStackAssignment,
-  LabelDefinition,
-  AssemblySwitch,
-  AssemblyCase,
-  AssemblyFunctionDefinition,
-  AssemblyFunctionReturns,
-  AssemblyFor,
-  AssemblyIf,
-  AssemblyLiteral,
-  SubAssembly,
-  TupleExpression,
-  TypeNameExpression,
   StringLiteral,
   NumberLiteral,
-  BooleanLiteral,
   Identifier,
-  BinaryOperation,
-  Conditional,
   IndexAccess,
   MemberAccess,
-  Break,
   HexNumber,
   DecimalNumber,
-  Continue,
 } from "@solidity-parser/parser";
+import { ConstantPool } from "./ConstantPool";
 
 /**
  * Visits the AST nodes of a contract to find all constants
@@ -69,265 +31,111 @@ import {
  * @author Mitchell Olsthoorn
  */
 export class ConstantVisitor implements SolidityVisitor {
-  protected values = [];
 
-  SourceUnit(node: SourceUnit): void {}
+  protected pool: ConstantPool;
 
-  ImportDirective(node: ImportDirective): void {}
-
-  PragmaDirective(node: PragmaDirective): void {}
-
-  ContractDefinition(node: ContractDefinition): void {
-    this.values.push(node.name);
+  constructor(pool: ConstantPool) {
+    this.pool = pool;
   }
 
-  InheritanceSpecifier(node: InheritanceSpecifier): void {}
+  public getConstantPool(): ConstantPool {
+    return this.pool;
+  }
 
-  StateVariableDeclaration(node: StateVariableDeclaration): void {}
+  ImportDirective(node: ImportDirective): void {
+    this.pool.addString(node.path);
+    node.symbolAliases.forEach((symbolAlias) => {
+      symbolAlias.forEach((alias) => {
+        this.pool.addString(alias);
+      })
+    });
+    this.pool.addString(node.unitAlias);
+  }
 
-  UsingForDeclaration(node: UsingForDeclaration): void {}
+  PragmaDirective(node: PragmaDirective): void {
+    this.pool.addString(node.name);
+    this.pool.addString(node.value);
+  }
 
-  StructDefinition(node: StructDefinition): void {}
+  ContractDefinition(node: ContractDefinition): void {
+    this.pool.addString(node.name);
+  }
+
+  UsingForDeclaration(node: UsingForDeclaration): void {
+    this.pool.addString(node.libraryName);
+  }
+
+  StructDefinition(node: StructDefinition): void {
+    this.pool.addString(node.name);
+  }
 
   ModifierDefinition(node: ModifierDefinition): void {
-    this.values.push(node.name);
+    this.pool.addString(node.name);
   }
 
   ModifierInvocation(node: ModifierInvocation): void {
-    this.values.push(node.name);
+    this.pool.addString(node.name);
   }
 
   FunctionDefinition(node: FunctionDefinition): void {
-    this.values.push(node.name);
+    this.pool.addString(node.name);
   }
 
-  EventDefinition(node: EventDefinition): void {}
+  EventDefinition(node: EventDefinition): void {
+    this.pool.addString(node.name);
+  }
 
-  EnumValue(node: EnumValue): void {}
+  EnumValue(node: EnumValue): void {
+    this.pool.addString(node.name);
+  }
 
-  EnumDefinition(node: EnumDefinition): void {}
+  EnumDefinition(node: EnumDefinition): void {
+    this.pool.addString(node.name);
+  }
 
   VariableDeclaration(node: VariableDeclaration): void {
-    this.values.push(node.name);
+    this.pool.addString(node.name);
   }
 
-  UserDefinedTypeName(node: UserDefinedTypeName): void {}
+  UserDefinedTypeName(node: UserDefinedTypeName): void {
+    this.pool.addString(node.namePath);
+  }
 
-  Mapping(node: Mapping): void {}
+  FunctionTypeName(node: FunctionTypeName): void {
+    this.pool.addString(node.stateMutability);
+    this.pool.addString(node.visibility);
+  }
 
-  ArrayTypeName(node: ArrayTypeName): void {}
-
-  FunctionTypeName(node: FunctionTypeName): void {}
-
-  Block(node: Block): void {}
-
-  ExpressionStatement(node: ExpressionStatement): void {}
-
-  IfStatement(node: IfStatement): void {}
-
-  WhileStatement(node: WhileStatement): void {}
-
-  ForStatement(node: ForStatement): void {}
-
-  InlineAssemblyStatement(node: InlineAssemblyStatement): void {}
-
-  DoWhileStatement(node: DoWhileStatement): void {}
-
-  ContinueStatement(node: ContinueStatement): void {}
-
-  BreakStatement(node: BreakStatement): void {}
-
-  ReturnStatement(node: ReturnStatement): void {}
-
-  EmitStatement(node: EmitStatement): void {}
-
-  ThrowStatement(node: ThrowStatement): void {}
-
-  VariableDeclarationStatement(node: VariableDeclarationStatement): void {}
-
-  ElementaryTypeName(node: ElementaryTypeName): void {}
-
-  AssemblyBlock(node: AssemblyBlock): void {}
-
-  AssemblyCall(node: AssemblyCall): void {}
-
-  AssemblyLocalDefinition(node: AssemblyLocalDefinition): void {}
-
-  AssemblyAssignment(node: AssemblyAssignment): void {}
-
-  AssemblyStackAssignment(node: AssemblyStackAssignment): void {}
-
-  LabelDefinition(node: LabelDefinition): void {}
-
-  AssemblySwitch(node: AssemblySwitch): void {}
-
-  AssemblyCase(node: AssemblyCase): void {}
-
-  AssemblyFunctionDefinition(node: AssemblyFunctionDefinition): void {}
-
-  AssemblyFunctionReturns(node: AssemblyFunctionReturns): void {}
-
-  AssemblyFor(node: AssemblyFor): void {}
-
-  AssemblyIf(node: AssemblyIf): void {}
-
-  AssemblyLiteral(node: AssemblyLiteral): void {}
-
-  SubAssembly(node: SubAssembly): void {}
-
-  TupleExpression(node: TupleExpression): void {}
-
-  TypeNameExpression(node: TypeNameExpression): void {}
+  ElementaryTypeName(node: ElementaryTypeName): void {
+    this.pool.addString(node.name);
+  }
 
   StringLiteral(node: StringLiteral): void {
-    this.values.push(node.value);
+    this.pool.addString(node.value);
   }
 
   NumberLiteral(node: NumberLiteral): void {
-    this.values.push(node.number);
+    this.pool.addNumber(parseInt(node.number));
   }
-
-  BooleanLiteral(node: BooleanLiteral): void {}
 
   Identifier(node: Identifier): void {
-    if (!["require", "_"].includes(node.name)) this.values.push(node.name);
+    if (!["require", "_"].includes(node.name)) this.pool.addString(node.name);
   }
 
-  BinaryOperation(node: BinaryOperation): void {}
-
-  Conditional(node: Conditional): void {}
-
-  IndexAccess(node: IndexAccess): void {}
-
-  MemberAccess(node: MemberAccess): void {}
-
-  Break(node: Break): void {}
-
-  HexNumber(node: HexNumber): void {}
-
-  DecimalNumber(node: DecimalNumber): void {}
-
-  Continue(node: Continue): void {}
-
-  "SourceUnit:exit"(node: SourceUnit): void {}
-
-  "PragmaDirective:exit"(node: PragmaDirective): void {}
-
-  "ImportDirective:exit"(node: ImportDirective): void {}
-
-  "ContractDefinition:exit"(node: ContractDefinition): void {}
-
-  "InheritanceSpecifier:exit"(node: InheritanceSpecifier): void {}
-
-  "StateVariableDeclaration:exit"(node: StateVariableDeclaration): void {}
-
-  "UsingForDeclaration:exit"(node: UsingForDeclaration): void {}
-
-  "StructDefinition:exit"(node: StructDefinition): void {}
-
-  "ModifierDefinition:exit"(node: ModifierDefinition): void {}
-
-  "ModifierInvocation:exit"(node: ModifierInvocation): void {}
-
-  "FunctionDefinition:exit"(node: FunctionDefinition): void {}
-
-  "EventDefinition:exit"(node: EventDefinition): void {}
-
-  "EnumValue:exit"(node: EnumValue): void {}
-
-  "EnumDefinition:exit"(node: EnumDefinition): void {}
-
-  "VariableDeclaration:exit"(node: VariableDeclaration): void {}
-
-  "UserDefinedTypeName:exit"(node: UserDefinedTypeName): void {}
-
-  "Mapping:exit"(node: Mapping): void {}
-
-  "ArrayTypeName:exit"(node: ArrayTypeName): void {}
-
-  "FunctionTypeName:exit"(node: FunctionTypeName): void {}
-
-  "Block:exit"(node: Block): void {}
-
-  "ExpressionStatement:exit"(node: ExpressionStatement): void {}
-
-  "IfStatement:exit"(node: IfStatement): void {}
-
-  "WhileStatement:exit"(node: WhileStatement): void {}
-
-  "ForStatement:exit"(node: ForStatement): void {}
-
-  "InlineAssemblyStatement:exit"(node: InlineAssemblyStatement): void {}
-
-  "DoWhileStatement:exit"(node: DoWhileStatement): void {}
-
-  "ContinueStatement:exit"(node: ContinueStatement): void {}
-
-  "BreakStatement:exit"(node: BreakStatement): void {}
-
-  "ReturnStatement:exit"(node: ReturnStatement): void {}
-
-  "EmitStatement:exit"(node: EmitStatement): void {}
-
-  "ThrowStatement:exit"(node: ThrowStatement): void {}
-
-  "VariableDeclarationStatement:exit"(
-    node: VariableDeclarationStatement
-  ): void {}
-
-  "ElementaryTypeName:exit"(node: ElementaryTypeName): void {}
-
-  "AssemblyBlock:exit"(node: AssemblyBlock): void {}
-
-  "AssemblyCall:exit"(node: AssemblyCall): void {}
-
-  "AssemblyLocalDefinition:exit"(node: AssemblyLocalDefinition): void {}
-
-  "AssemblyAssignment:exit"(node: AssemblyAssignment): void {}
-
-  "AssemblyStackAssignment:exit"(node: AssemblyStackAssignment): void {}
-
-  "LabelDefinition:exit"(node: LabelDefinition): void {}
-
-  "AssemblySwitch:exit"(node: AssemblySwitch): void {}
-
-  "AssemblyCase:exit"(node: AssemblyCase): void {}
-
-  "AssemblyFunctionDefinition:exit"(node: AssemblyFunctionDefinition): void {}
-
-  "AssemblyFunctionReturns:exit"(node: AssemblyFunctionReturns): void {}
-
-  "AssemblyFor:exit"(node: AssemblyFor): void {}
-
-  "AssemblyIf:exit"(node: AssemblyIf): void {}
-
-  "AssemblyLiteral:exit"(node: AssemblyLiteral): void {}
-
-  "SubAssembly:exit"(node: SubAssembly): void {}
-
-  "TupleExpression:exit"(node: TupleExpression): void {}
-
-  "TypeNameExpression:exit"(node: TypeNameExpression): void {}
-
-  "NumberLiteral:exit"(node: NumberLiteral): void {}
-
-  "BooleanLiteral:exit"(node: BooleanLiteral): void {}
-
-  "Identifier:exit"(node: Identifier): void {}
-
-  "BinaryOperation:exit"(node: BinaryOperation): void {}
-
-  "Conditional:exit"(node: Conditional): void {}
-
-  "IndexAccess:exit"(node: IndexAccess): void {}
-
-  "MemberAccess:exit"(node: MemberAccess): void {}
-
-  "HexNumber:exit"(node: HexNumber): void {}
-
-  "DecimalNumber:exit"(node: DecimalNumber): void {}
-
-  "Break:exit"(node: Break): void {}
-
-  "Continue:exit"(node: Continue): void {}
+  IndexAccess(node: IndexAccess): void {
+    // TODO: check for index numbers
+  }
+
+  MemberAccess(node: MemberAccess): void {
+    this.pool.addString(node.memberName);
+  }
+
+  HexNumber(node: HexNumber): void {
+    // TODO: check for addresses
+    this.pool.addString(node.value);
+  }
+
+  DecimalNumber(node: DecimalNumber): void {
+    this.pool.addNumber(parseFloat(node.value));
+  }
 }
