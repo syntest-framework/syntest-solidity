b8af3f714a96bef8e5b62a39d4903b2e217a1dd1
Dimitri <tweet.dimitri@gmail.com>
2022-12-12 15:45:15 +0100

    Update copyright headers project

--- src/testcase/statements/action/ActionStatement.ts
+++ src/testcase/statements/action/ActionStatement.ts
@@ -1,7 +1,7 @@
 /*
  * Copyright 2020-2021 Delft University of Technology and SynTest contributors
  *
- * This file is part of SynTest Framework.
+ * This file is part of SynTest Solidity.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
