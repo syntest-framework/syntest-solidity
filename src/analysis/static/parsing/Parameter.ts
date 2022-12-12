eefc567394936caaf41bb0ca857fa8cd7238b630
Dimitri <tweet.dimitri@gmail.com>
2022-12-12 15:42:48 +0100

    Update copyright headers year

--- src/analysis/static/parsing/Parameter.ts
+++ src/analysis/static/parsing/Parameter.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-[yyyy] Delft University of Technology and SynTest contributors
+ * Copyright 2020-2022 Delft University of Technology and SynTest contributors
  *
  * This file is part of SynTest Solidity.
  *
